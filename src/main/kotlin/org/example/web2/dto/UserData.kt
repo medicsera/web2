591e9ecf28084314b40c27ba1c393d3daf41446b
medicsera <anton.mirko.0550@gmail.com>
2026-02-19 17:28:07 +0700

    chose : complete tasks

---
+++ src/main/kotlin/org/example/web2/dto/UserData.kt
@@ -0,0 +1,6 @@
+package org.example.web2.dto
+
+data class UserData(
+    val name: String,
+    val surname: String
+)
---
+++ src/test/kotlin/org/example/web2spingboot/web2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.web2spingboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class web2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
