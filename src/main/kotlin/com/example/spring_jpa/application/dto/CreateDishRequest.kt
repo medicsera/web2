07876e73573ee265789477024cd01864bba0b256
medicsera <anton.mirko.0550@gmail.com>
2026-04-29 16:42:09 +0700

    refactor: добавлено ограничение на длину описания в DTO CreateDishRequest

--- src/main/kotlin/com/example/spring_jpa/application/dto/CreateDishRequest.kt
+++ src/main/kotlin/com/example/spring_jpa/application/dto/CreateDishRequest.kt
@@ -5,16 +5,13 @@ import java.math.BigDecimal
 
 
 data class CreateDishRequest(
-
     @field:NotBlank(message = "Name cannot be blank")
     val name: String,
 
+    @field:Size(max = 255, message = "Description must not exceed 255 characters")
     val description: String = "",
 
-    val isAvailable: Boolean = true,
-
     @field:NotNull(message = "Price cannot be null")
-
     @field:Positive(message = "Price must be positive")
     val price: BigDecimal,
 
