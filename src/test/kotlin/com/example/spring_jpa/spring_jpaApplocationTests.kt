e3a82edf705879116ccccf1f0c668cbcf2f12bcb
medicsera <anton.mirko.0550@gmail.com>
2026-05-18 12:15:29 +0700

    test : remove active profile

--- src/test/kotlin/com/example/spring_jpa/spring_jpaApplocationTests.kt
+++ src/test/kotlin/com/example/spring_jpa/spring_jpaApplocationTests.kt
@@ -2,10 +2,8 @@ package com.example.spring_jpa
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles("test")
 class SpringJpaApplicationTests {
 
     @Test
