9ec1e787ebaa321a6dfce29242cc08620fbf57cb
medicsera <anton.mirko.0550@gmail.com>
2026-05-19 19:02:56 +0700

    fix: delete null restaurant_id rows before setting NOT NULL in V6 migration

--- src/main/resources/db/migration/V6__fix_nullable_constraints.sql
+++ src/main/resources/db/migration/V6__fix_nullable_constraints.sql
@@ -2,6 +2,7 @@ UPDATE dishes SET description = '' WHERE description IS NULL;
 ALTER TABLE dishes ALTER COLUMN description SET NOT NULL;
 ALTER TABLE dishes ALTER COLUMN description SET DEFAULT '';
 
+DELETE FROM dishes WHERE restaurant_id IS NULL;
 ALTER TABLE dishes ALTER COLUMN restaurant_id SET NOT NULL;
 
 UPDATE orders SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
